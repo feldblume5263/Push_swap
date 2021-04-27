dfe73b0f3a7f7f5d9ace14443c9b26cc8450623e
junhpark <feldblume5263@gmail.com>
2021-04-27 16:52:03 +0900

    change name

--- makechecker.sh
+++ makechecker.sh
@@ -1,1 +1,1 @@
-gcc src/checker.c src/error.c src/get_next_line.c src/operation.c src/other_utils.c src/print_stack.c src/stack_utils.c src/stack.c -Iincludes/ -o checker src/libft.a
+gcc src/checker.c src/error.c src/set_stack.c src/operation.c src/other_utils.c src/print_stack.c src/stack_utils.c src/stack.c -Iincludes/ -o checker src/libft.a
