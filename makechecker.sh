5a63f3f0e2b0676be71587711d7f5eb945730e95
junhpark <feldblume5263@gmail.com>
2021-04-27 16:46:23 +0900

    update checker make

--- makechecker.sh
+++ makechecker.sh
@@ -1,1 +1,1 @@
-gcc src/* -Iincludes/ -o push_swap
+gcc src/checker.c src/error.c src/get_next_line.c src/operation.c src/other_utils.c src/print_stack.c src/stack_utils.c src/stack.c -Iincludes/ -o checker src/libft.a
