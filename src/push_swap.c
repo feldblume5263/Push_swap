236c6ce3d74083b79a2ae84eaa91493843301aef
feldblume5263 <feldblume5263@gmail.com>
2021-04-30 12:21:08 +0900

    update code

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,11 +6,52 @@
 /*   By: junhpark <junhpark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/27 16:53:09 by junhpark          #+#    #+#             */
-/*   Updated: 2021/04/29 17:50:41 by junhpark         ###   ########.fr       */
+/*   Updated: 2021/04/29 21:49:09 by junhpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int					get_smallest(t_stack *stack)
+{
+	t_stack			*new;
+	int				smallest;
+
+	smallest = stack->index;
+	new = stack;
+	while (new)
+	{
+		if (new->index < smallest)
+			smallest = new->index;
+		new = new->next;
+	}
+	return (smallest);
+}
+
+void				move_smallest_to_first(t_stack **stack)
+{
+	t_stack			*new;
+	int				count;
+	int				smallest;
+	int				idx;
+
+	new = *stack;
+	count = 0;
+	smallest = get_smallest(*stack);
+	while (new && new->index != smallest)
+	{
+		new = new->next;
+		count++;
+	}
+	count++;
+	idx = -1;
+	if (count < (stack_size(*stack) / 2))
+		while (++idx < count)
+			ft_rotate(NULL, stack, TAG_B);
+	else
+		while (++idx < stack_size(*stack) - count + 1)
+			ft_rev_rotate(NULL, stack, TAG_B);
+}
 
 int					check_swap_effect(t_stack **a, t_stack **b, int mark)
 {
@@ -30,7 +71,7 @@ void				a_to_b(t_stack **a, t_stack **b)
 {
 	int				mark;
 
-	while (1)//(mark = count_markup(*a)) != stack_size(*a))
+	while ((mark = count_markup(*a)) != stack_size(*a))
 	{
 		if (check_swap_effect(a, b, mark) == 1)
 			write(1, "sa\n", 3);
@@ -45,7 +86,6 @@ void				a_to_b(t_stack **a, t_stack **b)
 			write(1, "ra\n", 3);
 		}
 		print_stack_with_opt(*a, *b);
-		system("read");
 	}
 }
 
@@ -55,6 +95,11 @@ void				sorting(t_stack **a, t_stack **b)
 	while (!(check_order(*a) == 1 && stack_size(*b) == 0))
 	{
 		a_to_b(a, b);
+		move_smallest_to_first(a);
+		printf(">>\n");
+		print_stack_with_opt(*a, *b);
+		break ;
+
 	}
 }
 
@@ -69,6 +114,4 @@ int					main(int argc, char *argv[])
 	set_index(a);
 	set_markup(a);
 	sorting(&a, &b);
-	print_stack_with_opt(a, b);
-
 }
